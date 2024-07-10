b72bb285d342542dd0df6a5b939417bd4672f600
Brittney Epperson <beppers87@hotmail.com>
2024-07-10 12:59:43 -0500

    Added HashMap test cases

---
+++ index.js
@@ -0,0 +1,21 @@
+import HashMap from "./hashMap.js";
+
+const test = HashMap();
+test.set('apple', 'red');
+test.set('banana', 'yellow');
+test.set('carrot', 'orange');
+test.set('dog', 'brown');
+test.set('elephant', 'gray');
+test.set('frog', 'green');
+test.set('grape', 'purple');
+test.set('hat', 'black');
+test.set('ice cream', 'white');
+test.set('jacket', 'blue');
+test.set('kite', 'pink');
+test.set('lion', 'golden');
+
+test.set('elephant', 'small');
+test.set('kite', 'red');
+test.set('moon', 'silver');
+
+test.display();
